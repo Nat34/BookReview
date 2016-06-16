7a81975ea69221c50ab45df9a5634cee0d063c4b
Nat34 <nathan.a.thomas@gmail.com>
2016-06-16 14:41:53 -0500

    add book params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,22 @@
 class BooksController < ApplicationController
+	
 	def index
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to root_path
+	end
+
+	private
+
+	def book_params
+		pararms.require(:book).permit(:title, :descript, :author)
 	end
 end
